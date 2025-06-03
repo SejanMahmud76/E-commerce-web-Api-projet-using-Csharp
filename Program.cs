a65346832c2e80a26db8a8157e2571f61976b078
Rakibul-Hasan-Sezan <sejan16@cse.pstu.ac.bd>
2025-06-03 15:44:21 +0600

    Creating Simple Endpoints Get, post,put,delete

--- Program.cs
+++ Program.cs
@@ -1,41 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// REST API => GET, POST, PUT, DELETE
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    return "Api is working";
+});
+
+app.MapGet("/hello", () =>
+{
+    return "Get Method: Hello";
+});
+
+app.MapPost("/hello", () =>
+{
+    return "Post Method: Hello";
+});
+
+app.MapPut("/hello", () =>
+{
+    return "Put Method: Hello";
+});
+
+app.MapDelete("/hello", () =>
+{
+    return "Delete Method: Hello";
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
